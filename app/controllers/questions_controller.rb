6ae83ce0e8bee70963f8f8845b49487104dffeea
Mark <markallenagaton03@gmail.com>
2015-09-26 20:41:26 +0800

    backend part

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,6 @@
 class QuestionsController < ApplicationController
+  def start_scene
+    @question = Question.find(1) 
+    @answers = @question.answers
+  end
 end
--- db/migrate/20150926093519_create_questions.rb
+++ db/migrate/20150926093519_create_questions.rb
@@ -1,6 +1,8 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
+      t.string :text
+      t.integer :scene_number
 
       t.timestamps null: false
     end
