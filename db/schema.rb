58dd2ce5cb00b6f8e4a4ee3b433f0463081a33a0
Mark Agaton <markallenagaton03@gmail.com>
2015-09-26 21:29:24 +0800

    add scene1

--- db/schema.rb
+++ db/schema.rb
@@ -16,12 +16,14 @@ ActiveRecord::Schema.define(version: 20150926104121) do
   create_table "answers", force: :cascade do |t|
     t.string   "text"
     t.integer  "question_id"
+    t.integer  "scene_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
 
   create_table "questions", force: :cascade do |t|
     t.string   "text"
+    t.integer  "scene_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
