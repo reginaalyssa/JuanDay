4e38231c0070366e08f87d3652a4bebe72925ed2
Mark <markallenagaton03@gmail.com>
2015-09-26 20:41:26 +0800

    backend part

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150926093519) do
+ActiveRecord::Schema.define(version: 20150926104121) do
+
+  create_table "answers", force: :cascade do |t|
+    t.string   "text"
+    t.integer  "question_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "questions", force: :cascade do |t|
+    t.string   "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
